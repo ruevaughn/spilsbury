a33a334ec9fee9be8792c73c3fa77e2244b5c402
Chase Jensen <soccerchase@gmail.com>
2014-06-29 16:26:00 -0600

    Remove migration and reference to admin index

--- db/migrate/20140628231812_add_image_to_obituary.rb
+++
@@ -1,9 +0,0 @@
-class AddImageToObituary < ActiveRecord::Migration
-  def self.up
-    add_attachment :obituaries, :image
-  end
-
-  def self.down
-    remove_attachment :obituaries, :image
-  end
-end
