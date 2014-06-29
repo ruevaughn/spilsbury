d899b6194f24e39827cd004f55ddb53a4d4b2e77
Chase Jensen <soccerchase@gmail.com>
2014-06-29 13:45:43 -0600

    Add seed data with random dates and format in view

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,23 @@ require 'faker'
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def random_number
+  20 + rand(80)
+end
+
+def random_born_date
+  Date.today - (random_number).years - (random_number).days
+end
+
+def random_death_date
+  Date.today - (random_number).days
+end
+
 Admin.delete_all
 admin = Admin.new(email: 'test@test.com', password: 'test1234', password_confirmation: 'test1234')
 admin.save!
 
-Obituary.create(first_name: Faker::Name.first_name, middle_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-Obituary.create(first_name: Faker::Name.first_name, middle_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-Obituary.create(first_name: Faker::Name.first_name, middle_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+Obituary.delete_all
+10.times do
+  Obituary.create(first_name: Faker::Name.first_name, middle_name: Faker::Name.first_name, last_name: Faker::Name.last_name, born_date: random_born_date, death_date: random_death_date)
+end
