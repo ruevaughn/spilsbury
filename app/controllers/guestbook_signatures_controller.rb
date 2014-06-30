52cb1412f863d044c2861bfcf12daec0600fd80b
Chase Jensen <soccerchase@gmail.com>
2014-06-30 16:09:11 -0600

    Add guestbook signing

--- app/controllers/guestbook_signatures_controller.rb
+++ app/controllers/guestbook_signatures_controller.rb
@@ -1,63 +1,26 @@
 class GuestbookSignaturesController < ApplicationController
   before_action :set_guestbook_signature, only: [:show, :edit, :update, :destroy]
 
-  # GET /guestbook_signatures
-  # GET /guestbook_signatures.json
-  def index
-    @guestbook_signatures = GuestbookSignature.all
-  end
-
-  # GET /guestbook_signatures/1
-  # GET /guestbook_signatures/1.json
-  def show
-  end
-
   # GET /guestbook_signatures/new
   def new
     @guestbook_signature = GuestbookSignature.new
   end
 
-  # GET /guestbook_signatures/1/edit
-  def edit
-  end
-
   # POST /guestbook_signatures
   # POST /guestbook_signatures.json
   def create
-    @guestbook_signature = GuestbookSignature.new(guestbook_signature_params)
+    @guestbook = Guestbook.find(params[:guestbook_id])
+    @guestbook.guestbook_signatures.build(guestbook_signature_params)
+    # @guestbook_signature = GuestbookSignature.new(guestbook_signature_params)
 
     respond_to do |format|
-      if @guestbook_signature.save
-        format.html { redirect_to @guestbook_signature, notice: 'Guestbook signature was successfully created.' }
-        format.json { render :show, status: :created, location: @guestbook_signature }
+      if @guestbook.save
+        format.html { redirect_to @guestbook, notice: 'Guestbook signature was successfully created.' }
+        format.json { render :show, status: :created, location: @guestbook }
       else
         format.html { render :new }
-        format.json { render json: @guestbook_signature.errors, status: :unprocessable_entity }
+        format.json { render json: @guestbook.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /guestbook_signatures/1
-  # PATCH/PUT /guestbook_signatures/1.json
-  def update
-    respond_to do |format|
-      if @guestbook_signature.update(guestbook_signature_params)
-        format.html { redirect_to @guestbook_signature, notice: 'Guestbook signature was successfully updated.' }
-        format.json { render :show, status: :ok, location: @guestbook_signature }
-      else
-        format.html { render :edit }
-        format.json { render json: @guestbook_signature.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /guestbook_signatures/1
-  # DELETE /guestbook_signatures/1.json
-  def destroy
-    @guestbook_signature.destroy
-    respond_to do |format|
-      format.html { redirect_to guestbook_signatures_url, notice: 'Guestbook signature was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  resources :guestbook_signatures
 
 
   devise_for :admins
   root 'static#home'
 
   resources :obituaries, only: [:index, :show]
-  resources :guestbooks, only: [:show]
+  resources :guestbooks, only: [:show] do
+    resources :guestbook_signatures, only: [:new, :create]
+  end
 
   namespace :admin do
     get 'home', action: 'home', controller: 'static', as: :panel
