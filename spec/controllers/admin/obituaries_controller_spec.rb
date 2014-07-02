482e7af55ffabcaafa0b7ba970db006d86c4260f
Chase Jensen <soccerchase@gmail.com>
2014-07-02 17:02:31 -0600

    Controller tests that are failing

--- spec/controllers/admin/obituaries_controller_spec.rb
+++ spec/controllers/admin/obituaries_controller_spec.rb
@@ -7,4 +7,12 @@ describe Admin::ObituariesController do
       expect(assigns(:obituary)).to eq Obituary.new
     end
   end
+
+  describe "GET edit" do
+    it "assigns @obituary to the current obituary" do
+      obit = create(:obituary)
+      get :edit, params: {id: obit.id}
+      expect(assigns(:obituary)).to eq obit
+    end
+  end
 end
