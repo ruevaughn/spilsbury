23d68f368ca759d4957a9e5b8801c045ba763e5b
Chase Jensen <soccerchase@gmail.com>
2014-07-02 17:12:21 -0600

    Finished model specs for obituary

--- spec/models/obituary_spec.rb
+++ spec/models/obituary_spec.rb
@@ -12,6 +12,27 @@ describe Obituary do
     end
   end
 
+  describe "#born_date_to_s" do
+    it "Returns the birth date as a formatted string" do
+      obituary = build_stubbed(:obituary, born_date: Date.today - 40.years)
+      expect(obituary.born_date_to_s).to eq (Date.today - 40.years).strftime('%B %d, %Y')
+    end
+  end
+
+  describe "#death_date_to_s" do
+    it "Returns the birth date as a formatted string" do
+      obituary = build_stubbed(:obituary, death_date: Date.today)
+      expect(obituary.death_date_to_s).to eq Date.today.strftime('%B %d, %Y')
+    end
+  end
+
+  describe "#life_range" do
+    it "returns the life range" do
+      obituary = build_stubbed(:obituary, born_date: Date.today - 40.years, death_date: Date.today)
+      expect(obituary.life_range).to eq (Date.today - 40.years).strftime('%B %d, %Y') + " - " + Date.today.strftime('%B %d, %Y')
+    end
+  end
+
   describe ".ascending" do
     it "returns ascending order" do
       person1 = create(:obituary, death_date: Date.today - 1)
