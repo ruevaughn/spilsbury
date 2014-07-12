859d2a7de061a60a957c470bcbb6609fd85f63a6
Chase Jensen <soccerchase@gmail.com>
2014-07-12 00:53:45 -0600

    Not sure if I should be committing sphinx development conf

--- app/indices/obituary_index.rb
+++ app/indices/obituary_index.rb
@@ -1,7 +1,8 @@
 # http://pat.github.io/thinking-sphinx/indexing.html
 
 ThinkingSphinx::Index.define :obituary, with: :active_record do
-  indexes first_name, sortable: true
+  indexes first_name
   indexes middle_name
-  indexes last_name, sortable: true
+  indexes last_name
+  indexes death_date, sortable: true
 end
