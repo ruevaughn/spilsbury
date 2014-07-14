b63957e47d53265673a076732ac94fffe802fa7d
Chase Jensen <soccerchase@gmail.com>
2014-07-13 19:11:05 -0600

    Add Thinking Sphinx searcH

--- app/controllers/obituaries_controller.rb
+++ app/controllers/obituaries_controller.rb
@@ -1,10 +1,13 @@
 class ObituariesController < ApplicationController
   def index
-    @obituaries = Obituary.all.descending.page params[:page]
+    if params[:q]
+      @obituaries = Obituary.search params[:q]
+    else
+      @obituaries = Obituary.all.descending.page params[:page]
+    end
   end
 
   def show
     @obituary = Obituary.find(params[:id])
   end
-
 end
